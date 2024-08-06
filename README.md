30e9ba7c88370655d7bcfcc9fa73b71f8fd062fb
User <user@localhost.localdomain>
2024-08-06 14:52:30 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# passsh
+
+Bash script for generating passwords and pin codes
+
+# Usage
+
+```
+./pass.sh -c -d
+```
---
+++ pass.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+# Generate passwords and pin codes
+
+while getopts 'cd' OPTION; do
+
+case "$OPTION" in 
+
+c)
+strings /dev/urandom |
+grep -o '[[:alnum:]!$%&/()=?@_-]' |
+head -n 20 | # 20 character password
+tr -d '\n'; echo
+;;
+
+d)
+strings /dev/urandom |
+grep -o '[0-9]' |
+head -n 4 | # 4 digit PIN
+tr -d '\n'; echo
+;;
+
+?)
+echo 'Usage: $(basename $0) [-c] [-d]'
+exit 1
+;;
+
+esac
+
+done
