1eb1e2f8c1fbb03f528f746df40e7252cab0d412
Antruc <admin@antruc.dev>
2025-05-16 17:08:35 -0700

    Add error handling

--- pass.sh
+++ pass.sh
@@ -2,29 +2,31 @@
 
 # Generate passwords and pin codes
 
-while getopts 'cd' OPTION; do
-
-case "$OPTION" in 
-
-c)
-strings /dev/urandom |
-grep -o '[[:alnum:]!$%&/()=?@_-]' |
-head -n 20 | # 20 character password
-tr -d '\n'; echo
-;;
-
-d)
-strings /dev/urandom |
-grep -o '[0-9]' |
-head -n 4 | # 4 digit PIN
-tr -d '\n'; echo
-;;
-
-?)
-echo 'Usage: ./pass.sh [-c] [-d]'
-exit 1
-;;
-
-esac
-
-done
+usage()
+{
+    echo "Usage: ./pass.sh [-c] [-d]"
+}
+if [[ -n "$1" ]] then
+    while getopts "cd" OPTION; do
+        case "$OPTION" in 
+            c)
+                strings /dev/urandom |
+                grep -o "[[:alnum:]!$%&/()=?@_-]" |
+                head -n 20 | # 20 character password
+                tr -d "\n"; echo
+            ;;
+            d)
+                strings /dev/urandom |
+                grep -o "[0-9]" |
+                head -n 4 | # 4 digit PIN
+                tr -d "\n"; echo
+            ;;
+            ?)
+                usage
+                exit 1
+            ;;
+        esac
+    done
+else
+    usage
+fi
