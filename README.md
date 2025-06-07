a12fd0a5f8fe251840371a21bd4d7361e94d618a
Antruc <admin@antruc.dev>
2025-06-07 11:56:31 -0700

    Release version 2.0.0

--- README.md
+++ README.md
@@ -5,5 +5,11 @@ Bash script for generating passwords and pin codes
 # Usage
 
 ```
-./pass.sh -c -d
+Usage: pass.sh -p [length] -c [length]
+    
+Options:
+    -p [length]    Generate random password.
+    -c [length]    Generate random pin code.
+    -h             Prints usage and exits.
+    -v             Prints version and exits.
 ```
--- pass.sh
+++ pass.sh
@@ -2,31 +2,71 @@
 
 # Generate passwords and pin codes
 
-usage()
-{
-    echo "Usage: ./pass.sh [-c] [-d]"
+version=2.0.0
+password_length=""
+pin_length=""
+
+echo_usage() {
+    echo "Usage: pass.sh -p [length] -c [length]
+    
+Options:
+    -p [length]    Generate random password.
+    -c [length]    Generate random pin code.
+    -h             Prints usage and exits.
+    -v             Prints version and exits."
 }
-if [[ -n "$1" ]] then
-    while getopts "cd" OPTION; do
-        case "$OPTION" in 
-            c)
-                strings /dev/urandom |
-                grep -o "[[:alnum:]!$%&/()=?@_-]" |
-                head -n 20 | # 20 character password
-                tr -d "\n"; echo
-            ;;
-            d)
-                strings /dev/urandom |
-                grep -o "[0-9]" |
-                head -n 4 | # 4 digit PIN
-                tr -d "\n"; echo
-            ;;
-            ?)
-                usage
+
+generate_password() {
+    strings /dev/urandom |
+    grep -o "[[:alnum:]!$%&/()=?@_-]" |
+    head -n $password_length |
+    tr -d "\n"; echo
+}
+
+generate_pin() {
+    strings /dev/urandom |
+    grep -o '[0-9]' |
+    head -n $pin_length |
+    tr -d '\n'; echo
+}
+
+while getopts ':p:c:hv' OPTION; do
+    case "$OPTION" in
+        p)
+            if [[ ${OPTARG} =~ ^[0-9]+$ ]]; then
+                password_length="${OPTARG}"
+                generate_password 
+            else
+                echo "Error: argument must be a number"
+            fi
+        ;;
+        c)
+            if [[ ${OPTARG} =~ ^[0-9]+$ ]]; then
+                pin_length="${OPTARG}"
+                generate_pin 
+            else
+                echo "Error: argument must be a number"
+            fi
+        ;;
+        h)
+            echo_usage
+            if [ "$#" -ne 1 ]; then
                 exit 1
-            ;;
-        esac
-    done
-else
-    usage
+            fi
+        ;;
+        v)
+            echo $version
+            if [ "$#" -ne 1 ]; then
+                exit 1
+            fi
+        ;;
+        ?)
+            echo_usage
+            exit 1
+        ;;
+    esac
+done
+
+if [ $OPTIND -eq 1 ]; then
+    echo_usage
 fi
